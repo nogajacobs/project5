295fbb4e9a97d147143c8e83f87767b07208e554
נעם ברוכי <nbaruchi@g.jct.ac.il>
2024-06-21 17:29:02 +0300

    start login

---
+++ src/components/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const login = () => {
+
+
+    return (
+        <form>
+          <label>username:
+            <input type="text" />
+          </label>
+          <label>password:
+            <input type="password" />
+          </label>
+        </form>
+      )
+}
